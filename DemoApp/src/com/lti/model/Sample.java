8f94bbad1d71ad27d3356621b726cfc0ffdfdb0c
'Kartikkadam27' <'kkadam097@gmail.com'>
2020-12-18 16:06:32 +0530

    First commit

---
+++ DemoApp/src/com/lti/model/Sample.java
@@ -0,0 +1,10 @@
+package com.lti.model;
+
+public class Sample {
+	public void show() {
+		System.out.println("Hello world");
+	}
+	public void meathod() {
+		System.out.println("Hello all");
+	}
+}
